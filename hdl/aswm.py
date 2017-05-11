b632a25779dd3ca7b5bb7e719f55e6e3f9844379
lamazavr <mr.dimas@meta.ua>
2017-05-11 17:46:19 +0300

    initial

--- hdl/aswm.py
+++ hdl/aswm.py
@@ -5,7 +5,7 @@ def Multiplier(clk, x, w, wx):
 
     @always(clk.posedge)
     def MultiplierLogic():
-        wx.next = (w * x) >> 16
+        wx.next = (w * x << 16) >> 16
 
     return MultiplierLogic
 
@@ -56,8 +56,7 @@ def WMean(clk,
     # multiplying each input
     mull_inst = []
     for i in range(9):
-        mull_inst.append(Multiplier(clk, x[i] << 16, w[i], add_l_0[i]))
-
+        mull_inst.append(Multiplier(clk, x[i], w[i], add_l_0[i]))
 
     # Calculation of weighted sum in 4clks
     wacc = Signal(intbv(0, min=w0.min, max=w0.max))
@@ -78,7 +77,6 @@ def WMean(clk,
     reg_inst_0 = Register(clk, add_l_0[8], add_l_1[4])
     reg_inst_1 = Register(clk, add_l_1[4], add_l_2[2])
     reg_inst_2 = Register(clk, add_l_2[2], add_l_3[1])
-
 
     # Calculation of weights sum in 4clks
     # Init signals with 1 to prevent div by 0
@@ -105,78 +103,7 @@ def WMean(clk,
     reg_inst_4 = Register(clk, wsum_l_0[4], wsum_l_1[2])
     reg_inst_5 = Register(clk, wsum_l_1[2], wsum_l_2[1])
 
-
     # Calculate weighted mean
     div_inst = FracDiv(clk, wacc, acc, wmean)
 
     return instances()
-
-
-def wmean_testbench():
-    clock = Signal(bool(0))
-
-    x = [i for i in range(9)]
-    x[0] = Signal(intbv(0x39, min=0, max=2**8))
-    x[1] = Signal(intbv(0x45, min=0, max=2**8))
-    x[2] = Signal(intbv(0x3c, min=0, max=2**8))
-    x[3] = Signal(intbv(0x00, min=0, max=2**8))
-    x[4] = Signal(intbv(0x83, min=0, max=2**8))
-    x[5] = Signal(intbv(0x00, min=0, max=2**8))
-    x[6] = Signal(intbv(0x60, min=0, max=2**8))
-    x[7] = Signal(intbv(0x86, min=0, max=2**8))
-    x[8] = Signal(intbv(0x00, min=0, max=2**8))
-
-    w = [Signal(intbv(0x00010000, min=0, max=2**32)) for i in range(9)]
-    wmean = Signal(intbv(0, min=0, max=2**32))
-
-    cmp_1 = WMean(clock, x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8],
-                  w[0], w[1], w[2], w[3], w[4], w[5], w[6], w[7], w[8],
-                  wmean)
-
-    HALF_PERIOD = delay(10)
-
-    @always(HALF_PERIOD)
-    def clockGen():
-        clock.next = not clock
-
-    @instance
-    def stimulus():
-        # wait for pipeline
-        for i in range(8):
-            yield clock.posedge
-
-        raise StopSimulation
-
-    @instance
-    def monitor():
-
-        # wait for pipeline
-        for i in range(6):
-            yield clock.posedge
-
-        yield clock.posedge
-        assert(wmean == 0x3cc71c), "Error weighted mean calculation"
-
-
-    return clockGen, stimulus, cmp_1, monitor
-
-
-if __name__ == "__main__":
-    from myhdl import traceSignals
-
-    #tb = traceSignals(testbench)
-    tb = wmean_testbench()
-    Simulation(tb).run()
-
-    #print(wmean)
-
-    # cmp_1 = toVHDL(WMean, clock,
-    #                x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8],
-    #                w[0], w[1], w[2], w[3], w[4], w[5], w[6], w[7], w[8],
-    #                wmean)
-
-    # a = Signal(intbv(0, min=0, max=2**16))
-    # b = Signal(intbv(0, min=0, max=2**16))
-    # res = Signal(intbv(0, min=0, max=2**16))
-    #
-    # cmp_2 = toVHDL(FracDiv, clock, a, b, res)
